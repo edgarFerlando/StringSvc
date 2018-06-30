a89d8ea3a41398911541db6c39e58ad99c123609
fendijatmiko <jatmikofendi@gmail.com>
2018-06-30 15:35:19 +0700

    add endpoint

---
+++ stringsvc/endpoint.go
@@ -0,0 +1,21 @@
+package stringsvc 
+
+// Endpoints are a primary abstraction in go-kit. An endpoint represents a single RPC (method in our service interface)
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(ctx, req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, ""}, nil
+	}
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(ctx, req.S)
+		return countResponse{v}, nil
+	}
+}
